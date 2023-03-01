81c0ad9867732d5dedac95e83050ba6ff3628526
MiguelCruz03 <126091723+MiguelCruz03@users.noreply.github.com>
2023-03-01 14:14:10 +0000

    Add files via upload

---
+++ Github/src/git/HelloGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package git;
+
+/**
+ * @author alunoESGTS
+ *
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+		System.out.println("Hello Git");
+	}
+
+}
